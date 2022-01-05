ee1bbfae3bc44fdcbcd1834880601a2d777a4d8c
laismune <aymune@hotmail.com>
2022-01-05 15:28:55 -0300

    separaçao da variavel artigos

--- index.php
+++ index.php
@@ -1,23 +1,7 @@
 <?php
-    $artigos = [
-    ['titulo' => 'Primeiro passos com Spring',
-    'conteudo' => 'Na empresa onde trabalho começamos um Coding Dojo, que é basicamente uma reunião com programadores e
-    programadoras a fim de resolver desafios e aperfeiçoar as habilidades com algoritmos.',
-    'link' => 'primeiros-passos-com-spring.html'],
-
-    ['titulo' => 'O que é Metodologia Ágil?',
-    'conteudo' => 'Uma vez fui contratada por uma empresa que desenvolvia softwares e aplicativos para outras empresas.',
-    'link' => 'o-que-e-metodologia-agil.html'],
-
-    ['titulo' => 'Como é o funil do Growth Hacking?',
-    'conteudo' => 'Minha amiga que possui um clube de assinaturas começou a utilizar o Growth Hacking após conhecer um pouco
-    mais sobre ele.',
-    'link' => 'como-e-o-funil-do-growth-hacking.html'],
-
-    ['titulo' => 'Novo Post',
-    'conteudo' => 'Conteúdo do novo post.',
-    'link' => 'arquivo_do_novo_post.html']
-    ];
+    include 'artigo.php';
+    $artigo = new Artigo();
+    $artigos = $artigo -> exibirTodos();
 ?>
 
 <!DOCTYPE html>
